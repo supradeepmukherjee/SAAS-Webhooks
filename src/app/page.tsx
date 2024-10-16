a7a003130f098dd265bd5f5add2dd19338915a87
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-10-17 00:04:00 +0530

    Created Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@clerk/nextjs/server";
+import { GitHubLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { Clock, List, Users } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const { userId } = auth()
+	if (userId) redirect('/dashboard')
+	return (
+		<div className="flex flex-col min-h-screen from-background to-primary/10 bg-gradient-to-b">
+			<div className="container flex-grow px-4 py-12 mx-auto">
+				<Card className="mb-12 border-none from-primary/20 to-secondary/20 bg-gradient-to-r">
+					<CardHeader>
+						<CardTitle className="text-center text-transparent text-5xl font-extrabold from-primary to-secondary bg-gradient-to-r bg-clip-text">
+							Welcome to TODOhub
+						</CardTitle>
+					</CardHeader>
+					<CardContent>
+						<p className="text-center mb-8 text-muted-foreground text-xl">
+							TODOhub - Stay Organized, Achieve More – Your Tasks Simplified!
+						</p>
+						<div className="flex justify-center space-x-6">
+							<Button asChild size='lg' className="text-lg">
+								<Link href='/register'>
+									Start for Free
+								</Link>
+							</Button>
+							<Button asChild size='lg' className="text-lg" variant='outline'>
+								<Link href='/login'>
+									Login
+								</Link>
+							</Button>
+						</div>
+					</CardContent>
+				</Card>
+				<Card className="mb-12 border-none">
+					<CardHeader>
+						<CardTitle className="text-center text-primary text-3xl font-bold">
+							Efficient Task Management with valuable Features
+						</CardTitle>
+					</CardHeader>
+					<CardContent>
+						<div className="grid md:grid-cols-3 gap-8">
+							<div className="flex flex-col items-center text-center p-6 bg-card rounded-lg shadow-md hover:shadow-lg transition-shadow">
+								<List className="h-16 w-16 text-primary mb-4" />
+								<h4 className="text-xl font-semibold mb-3">
+									Smart Organisation
+								</h4>
+								<p className="text-muted-foreground">
+									Categorise & Schedule Tasks with our easy-to-use Interface
+								</p>
+							</div>
+							<div className="flex flex-col items-center text-center p-6 bg-card rounded-lg shadow-md hover:shadow-lg transition-shadow">
+								<Clock className="h-16 w-16 text-primary mb-4" />
+								<h4 className="text-xl font-semibold mb-3">
+									Unforgettable Reminders
+								</h4>
+								<p className="text-muted-foreground">
+									Never miss Deadlines with our reminders which will try to make tasks unforgettable for you
+								</p>
+							</div>
+							<div className="flex flex-col items-center text-center p-6 bg-card rounded-lg shadow-md hover:shadow-lg transition-shadow">
+								<Users className="h-16 w-16 text-primary mb-4" />
+								<h4 className="text-xl font-semibold mb-3">
+									Effortless Collaboration
+								</h4>
+								<p className="text-muted-foreground">
+									Work Collectively and complete tasks at a Rapid Pace
+								</p>
+							</div>
+						</div>
+					</CardContent>
+				</Card>
+				<Card className="mb-12 border-none from-primary/20 to-secondary/20 bg-gradient-to-r">
+					<CardHeader>
+						<CardTitle className="text-3xl font-bold text-center">
+							What our Users say
+						</CardTitle>
+					</CardHeader>
+					<CardContent>
+						<div className="grid md:grid-cols-2 gap-6">
+							<blockquote className="italic text-lg text-muted-foreground">
+								&ldquo;TODOhub has transformed the way our team manages projects. It&apos;s intuitive, powerful, and valuable!&rdquo;
+								<footer className="text-right font-semibold mt-2">
+									Suresh Bhai., Project Manager
+								</footer>
+							</blockquote>
+							<blockquote className="italic text-lg text-muted-foreground">
+								&ldquo;I&apos;ve tried many task management apps, but TODOhub is by far the best. It&apos;s boosted my productivity tenfold!&rdquo;
+								<footer className="text-right font-semibold mt-2">
+									Desi Vyapari., Founder
+								</footer>
+							</blockquote>
+						</div>
+					</CardContent>
+				</Card>
+			</div>
+			<footer className="bg-muted py-8">
+				<div className="flex flex-col md:flex-row justify-between items-center container px-4 mx-auto">
+					<div className="flex justify-center space-x-6 mb-4 md:mb-0">
+						<a href="https://github.com/supradeepmukherjee" className="text-muted-foreground hover:text-primary transition-colors">
+							<span className="sr-only">
+								Github
+							</span>
+							<GitHubLogoIcon className='h-8 w-8' />
+						</a>
+						<a href="https://twitter.com/supradeep2004" className="text-muted-foreground hover:text-primary transition-colors">
+							<span className="sr-only">
+								Twitter
+							</span>
+							<TwitterLogoIcon className='h-8 w-8' />
+						</a>
+						<a href="https://www.linkedin.com/in/supradeep-mukherjee/" className="text-muted-foreground hover:text-primary transition-colors">
+							<span className="sr-only">
+								Linkedin
+							</span>
+							<LinkedInLogoIcon className='h-8 w-8' />
+						</a>
+					</div>
+					<p className="text-center text-sm text-muted-foreground">
+						&copy; {new Date().getFullYear()} TODOhub. All Rights Reserved
+						<Link href='/privacy' className='hover:underline'>
+							Privacy Policy
+						</Link>
+						|
+						<Link href='/terms' className='hover:underline'>
+							Terms of Service
+						</Link>
+					</p>
+				</div>
+			</footer>
+		</div>
+	)
 }
